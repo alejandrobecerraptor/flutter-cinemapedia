2a57d4f5b5c7a15975332b75f9249cc7b4ec797a
Alejandro Bece <alejandro.becerra.pro@gmail.com>
2024-10-03 16:04:27 -0500

    varios llamados y infinite scroll horizontal

--- lib/presentation/providers/movies/movies_providers.dart
+++ lib/presentation/providers/movies/movies_providers.dart
@@ -22,12 +22,12 @@ class MoviesNotifier extends StateNotifier<List<Movie>> {
 
   Future<void> loadNextPage() async {
     if (isLoading) return;
-    isLoading = true;
 
+    isLoading = true;
     currentPage++;
     final List<Movie> movies = await fetchMoreMovies(page: currentPage);
     state = [...state, ...movies];
-    
+    await Future.delayed(const Duration(milliseconds: 300));
     isLoading = false;
   }
 }
